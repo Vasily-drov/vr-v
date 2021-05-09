cb0150aee49d8a1158391c241944dea0448d1685
Vasily-drov <83358552+Vasily-drov@users.noreply.github.com>
2021-05-09 04:02:53 +0300

    Add files via upload

---
+++ public/components/nft.js
@@ -0,0 +1,28 @@
+var extendDeep = AFRAME.utils.extendDeep;
+var meshMixin = AFRAME.primitives.getMeshMixin();
+
+AFRAME.registerPrimitive('a-nft', extendDeep({}, meshMixin, {
+    defaultComponents: {
+        geometry: {primitive: 'plane'},
+        img_url: {},
+    },
+    mappings: {
+        height: 'geometry.height',
+        width: 'geometry.width',
+        src: 'material.src'
+    }
+}));
+
+AFRAME.registerComponent('img_url', {
+    schema: {
+        img: {type: 'string'},
+    },
+    init: function () {
+        setTimeout(() => {
+            var el = this.el;
+            this.data.img = links.pop();
+           // console.log(this.data.img);
+            el.setAttribute('src', this.data.img);
+        }, 6000);
+    }
+})
